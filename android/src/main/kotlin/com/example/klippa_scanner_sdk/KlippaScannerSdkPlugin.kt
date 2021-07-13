374102ae90258a573ba50651f383bc71ceef94ac
Robin Farmer <robin@klippa.com>
2021-07-13 15:10:04 +0200

    Added Android support.

--- android/src/main/kotlin/com/example/klippa_scanner_sdk/KlippaScannerSdkPlugin.kt
+++ android/src/main/kotlin/com/example/klippa_scanner_sdk/KlippaScannerSdkPlugin.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import androidx.annotation.NonNull
 import com.klippa.scanner.KlippaScanner
 import com.klippa.scanner.`object`.Image
+import android.util.Size
 import android.widget.Toast
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
@@ -85,17 +86,120 @@ class KlippaScannerSdkPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, P
         KlippaScanner.license = call.argument<String>("License")!!
       }
 
+      if (call.hasArgument("AllowMultipleDocuments")) {
+        KlippaScanner.menu.allowMultiDocumentsMode = call.argument<Boolean>("AllowMultipleDocuments")!!
+      }
+
+      if (call.hasArgument("DefaultMultipleDocuments")) {
+        KlippaScanner.menu.isMultiDocumentsModeEnabled = call.argument<Boolean>("DefaultMultipleDocuments")!!
+      }
+
+      if (call.hasArgument("DefaultCrop")) {
+        KlippaScanner.menu.isCropEnabled = call.argument<Boolean>("DefaultCrop")!!
+      }
+
+      if (call.hasArgument("MoveCloserMessage")) {
+        KlippaScanner.messages.moveCloserMessage = call.argument<String>("MoveCloserMessage")!!
+      }
+
+      if (call.hasArgument("ImageMaxWidth")) {
+        KlippaScanner.images.resolutionMaxWidth = call.argument<Int>("ImageMaxWidth")!!
+      }
+
+      if (call.hasArgument("ImageMaxHeight")) {
+        KlippaScanner.images.resolutionMaxWidth = call.argument<Int>("ImageMaxHeight")!!
+      }
+
+      if (call.hasArgument("ImageMaxQuality")) {
+        KlippaScanner.images.outputQuality = call.argument<Int>("ImageMaxQuality")!!
+      }
+
+      if (call.hasArgument("PreviewDuration")) {
+        KlippaScanner.durations.previewDuration = call.argument<Double>("PreviewDuration")!!
+      }
+
+      if (call.hasArgument("Model.fileName")) {
+        KlippaScanner.model.modelName = call.argument<String>("Model.fileName")!!
+      }
+
+      if (call.hasArgument("Model.modelLabels")) {
+        KlippaScanner.model.modelLabels = call.argument<String>("Model.modelLabels")!!
+      }
+
+      if (call.hasArgument("Timer.enabled")) {
+        KlippaScanner.menu.isTimerEnabled = call.argument<Boolean>("Timer.enabled")!!
+      }
+
+      if (call.hasArgument("Timer.duration")) {
+        KlippaScanner.durations.timerDuration = call.argument<Double>("Timer.duration")!!
+      }
+
+      var height: Int = 0
+      var width: Int = 0
+
+      if (call.hasArgument("CropPadding.height")) {
+        height = call.argument<Int>("CropPadding.height")!!
+      }
+
+      if (call.hasArgument("CropPadding.width")) {
+        width = call.argument<Int>("CropPadding.width")!!
+      }
+
+      KlippaScanner.images.cropPadding = Size(width, height)
+
+      if (call.hasArgument("Success.previewDuration")) {
+        KlippaScanner.durations.successPreviewDuration = call.argument<Double>("Success.previewDuration")!!
+      }
+
+      if (call.hasArgument("Success.message")) {
+        KlippaScanner.messages.successMessage = call.argument<String>("Success.message")!!
+      }
+
+      if (call.hasArgument("ShutterButton.allowshutterButton")) {
+        KlippaScanner.shutterButton.allowShutterButton = call.argument<Boolean>("ShutterButton.allowshutterButton")!!
+      }
+
+      if (call.hasArgument("ShutterButton.hideShutterbutton")) {
+        KlippaScanner.shutterButton.hideShutterButton = call.argument<Boolean>("ShutterButton.hideShutterbutton")!!
+      }
+
+      if (call.hasArgument("StoragePath")) {
+        KlippaScanner.images.outputDirectory = call.argument<String>("StoragePath")!!
+      }
+
+      if (call.hasArgument("DefaultColor")) {
+        val default = call.argument<String>("DefaultColor")!!
+        if (default == "original") {
+          KlippaScanner.colors.defaultColor = KlippaScanner.DefaultColor.ORIGINAL
+        } else {
+          KlippaScanner.colors.defaultColor = KlippaScanner.DefaultColor.GRAYSCALE
+        }
+      }
+
+      if (call.hasArgument("ImageLimit")) {
+        KlippaScanner.images.imageLimit = call.argument<Int>("ImageLimit")!!
+      }
+
+      if (call.hasArgument("ImageLimitReachedMessage")) {
+        KlippaScanner.messages.imageLimitReached = call.argument<String>("ImageLimitReachedMessage")!!
+      }
+
+      if (call.hasArgument("OutputFilename")) {
+        KlippaScanner.images.outputFileName = call.argument<String>("OutputFilename")!!
+      }
+
+      if (call.hasArgument("ImageMovingSensitivityAndroid")) {
+        KlippaScanner.images.imageMovingSensitivity = call.argument<Int>("ImageMovingSensitivityAndroid")!!
+      }
+
       val klippaScannerIntent = Intent(context, KlippaScanner::class.java)
       resultHandler = result
       activityPluginBinding!!.addActivityResultListener(this)
       activityPluginBinding!!.activity.startActivityForResult(klippaScannerIntent, SESSION_REQUEST_CODE)
 
-
-
     } catch (e: Exception) {
       result.error(E_FAILED_TO_SHOW_SESSION, "Could not launch scanner session", e.message + "\n" + Log.getStackTraceString(e))
     }
-
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
