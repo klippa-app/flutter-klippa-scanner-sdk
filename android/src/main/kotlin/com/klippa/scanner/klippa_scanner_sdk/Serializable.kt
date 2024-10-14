6b04ec84fabdd294613b696abbcd073366d7f433
Robin Farmer <robin@klippa.com>
2024-10-14 10:26:48 +0200

    bump sdk version to 4.0.0+ and bump gradle versions.

---
+++ android/src/main/kotlin/com/klippa/scanner/klippa_scanner_sdk/Serializable.kt
@@ -0,0 +1,14 @@
+package com.klippa.scanner.klippa_scanner_sdk
+
+import android.content.Intent
+import android.os.Build
+import java.io.Serializable
+
+internal inline fun <reified T : Serializable> Intent.serializable(key: String): T? =
+    when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializableExtra(key, T::class.java)
+        else -> {
+            @Suppress("DEPRECATION")
+            getSerializableExtra(key) as? T
+        }
+    }
